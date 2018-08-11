751824146d788d6f6a99e0cdaef840980858d190
Nesh108 <albertovaccari108@gmail.com>
2018-08-11 19:19:41 +0200

    Created login and about page

---
+++ src/pages/About.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello container">
+    <TopNavBar/>
+    <br>
+    <div class="jumbotron">
+        <h3 class="beta-title">About</h3>
+        <br>
+        <div class="beta-text">
+            <p>
+                yadayadayada yadayadayada
+            </p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TopNavBar from '../components/TopNavBar.vue'
+
+export default {
+  name: 'About',
+  components: {
+      TopNavBar
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
---
+++ src/pages/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <TopNavBar/>
+    <br>
+    <div class="jumbotron">
+        <h3 class="beta-title">Login</h3>
+        <br>
+        <form class="beta-text">
+            <div class="form-group row">
+                <label for="email" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-6">
+                <input type="email" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="password" class="col-sm-2 col-form-label">Password</label>
+                <div class="col-sm-6">
+                <input type="password" class="form-control"/>
+                </div>
+            </div>
+        </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import TopNavBar from '../components/TopNavBar.vue'
+
+export default {
+  name: 'Login',
+  components: {
+      TopNavBar
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
