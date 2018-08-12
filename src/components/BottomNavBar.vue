a8151bcb22ca8f707e802b9fdb695ff1d4561cb2
Lily <lily.bickerstaffe@live.co.uk>
2018-08-12 12:43:30 +0530

    Created bottom nav bar

---
+++ src/components/BottomNavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <nav class="navbar navbar-light bg-light big-margin beta-innerbar">
+            <button class="btn btn-outline-info" type="button" disabled>Previous</button>
+            <span class="navbar-text">
+                <button class="btn btn-outline-info" type="button" disabled>Next</button>
+            </span>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BottomNavBar'
+}
+</script>
