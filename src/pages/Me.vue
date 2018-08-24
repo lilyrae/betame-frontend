7ed206ec98e048d37899411030d0b71960b46a31
Lily <lily.bickerstaffe@live.co.uk>
2018-08-24 15:47:53 +0530

    Created page for user account

---
+++ src/pages/Me.vue
@@ -0,0 +1,21 @@
+<template>
+  <Jumbotron>
+    <span slot="title">MEME</span>
+    <div>
+        <p>
+           MEMEME
+        </p>
+    </div>
+  </Jumbotron>
+</template>
+
+<script>
+import Jumbotron from '../templates/Jumbotron.vue'
+
+export default {
+  name: 'Me',
+  components: {
+      Jumbotron
+  }
+}
+</script>
