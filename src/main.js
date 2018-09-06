46ce2794fb9a6732cfea66f89510c451bd60970c
Lily <lily.bickerstaffe@live.co.uk>
2018-09-06 20:02:36 +0530

    Fixed routes and events

--- src/main.js
+++ src/main.js
@@ -2,21 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import vSelect from 'vue-select'
 import VueRouter from 'vue-router'
-import StoryList from './pages/StoryList.vue'
+import Home from './pages/Home.vue'
 import Login from './pages/Login.vue'
 import Me from './pages/Me.vue'
 import About from './pages/About.vue'
 import CreateStory from './pages/CreateStory.vue'
+import Story from './pages/Story.vue'
 
 Vue.component('v-select', vSelect)
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', component: StoryList },
+  { path: '/', component: Home },
   { path: '/login', component: Login },
   { path: '/me', component: Me },
   { path: '/about', component: About },
-  { path: '/new', component: CreateStory }
+  { path: '/story/new', component: CreateStory },
+  { path: '/story/:id', component: Story, props: true }
 ]
 
 const router = new VueRouter({
@@ -24,6 +26,9 @@ const router = new VueRouter({
 })
 
 Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+window.Event = new Vue();
 
 new Vue({
   ...App,
