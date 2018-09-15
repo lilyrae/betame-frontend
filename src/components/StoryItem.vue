3ff0b330b245e43434b18bc0fcb1edcf151449e4
Lily <lily.bickerstaffe@live.co.uk>
2018-09-16 02:02:14 +0530

    Restyled story items and fixed displaying tags

--- src/components/StoryItem.vue
+++ src/components/StoryItem.vue
@@ -1,25 +1,36 @@
 <template>
     <li class="list-group-item beta-text">
-        <h4>
-        <a :href="story.url" target="_blank">{{ story.title }} </a>
-        <span class="font18 grey">by {{ story.user.username }}</span>
-        <span class="beta-title font14 float-right">{{ story.created_at | formatDate }}</span>
-        </h4>
-        <p>{{ story.notes }}</p>
-        <p 
-        v-for="tag in story.tags"
-        v-bind:key="tag.id"
-        v-bind:class="{
-            'beta-grey-badge': tag.type == 1,
-            'beta-blue-badge': tag.type == 2,
-            'beta-dark-blue-badge': tag.type == 3,
-            'badge-info': tag.type == 4
-        }"
-        class="badge small-margin">
-            {{ tag.label }}
-        </p>
-        <p><i>{{ story.word_count }} words</i></p>
-        <slot></slot>
+        <div class="row">
+        <div class="bullet-point-col">
+            <i class="fas fa-pen-nib text-danger font14"></i>
+        </div>
+        <div class="content-col">
+            <h4>
+            <a :href="storyUrl" class="beta-title" target="_blank">{{ story.title }}</a>&nbsp;
+            <span class="font18 grey">by {{ story.user.username }}</span>
+            <span class="beta-title font14 float-right">{{ story.created_at | formatDate }}</span>
+            </h4>
+            <div class="font18">
+                <p>
+                    <span
+                    v-for="tag in story.tags"
+                    v-bind:key="tag.tag_id"
+                    v-bind:class="{
+                        'beta-grey-badge': tag.tag_type_id == 1,
+                        'beta-blue-badge': tag.tag_type_id == 2,
+                        'beta-dark-blue-badge': tag.tag_type_id == 3,
+                        'badge-info': tag.tag_type_id == 4
+                    }"
+                    class="badge small-margin">
+                        {{ tag.text }}
+                    </span>
+                </p>
+                <p>{{ story.notes }}</p>
+                <p><i>{{ story.word_count }} words</i></p>
+            </div>
+            <slot></slot>
+        </div>
+        </div>
     </li>
 </template>
 
@@ -35,6 +46,23 @@ export default {
         formatDate(date) {
             return moment(date).format('Do MMMM YYYY');
         }
+    },
+    computed: {
+        storyUrl() {
+            return "/#/story/" + this.story.story_id;
+        }
     }
 }
 </script>
+
+<style scoped>
+.bullet-point-col {
+    width: 3%;
+    padding: 7px;
+}
+
+.content-col {
+    width: 95%;
+    padding: 5px;
+}
+</style>
--- src/pages/Story.vue
+++ src/pages/Story.vue
@@ -1,30 +1,28 @@
 <template>
   <Jumbotron class="text-left">
-    <div slot="title">{{ story.title }} 
+    <div slot="title">
+        {{ story.title }}
         <span class="font18 beta-text"> by {{ story.user.username }}</span>
         <span class="font14 float-right">{{ story.created_at | formatDate }}</span>
-        <br>
     </div>
     <div slot="subtitle">
-    <h5><a class="text-info" :href="story.url">{{ story.url }}</a></h5>
-    <hr>
+        <h5><a class="text-info" :href="story.url">{{ story.url }}</a></h5>
+        <hr>
     </div>
     <div>
-        <p>
-            {{ story.notes }}
-        </p>
         <p 
         v-for="tag in story.tags"
-        v-bind:key="tag.id"
+        v-bind:key="tag.tag_id"
         v-bind:class="{
-            'beta-grey-badge': tag.type == 1,
-            'beta-blue-badge': tag.type == 2,
-            'beta-dark-blue-badge': tag.type == 3,
-            'badge-info': tag.type == 4
+            'beta-grey-badge': tag.tag_type_id == 1,
+            'beta-blue-badge': tag.tag_type_id == 2,
+            'beta-dark-blue-badge': tag.tag_type_id == 3,
+            'badge-info': tag.tag_type_id == 4
         }"
         class="badge small-margin">
-            {{ tag.label }}
+            {{ tag.text }}
         </p>
+        <p>{{ story.notes }}</p>
         <p><i>{{ story.word_count }} words</i></p>
     </div>
   </Jumbotron>
