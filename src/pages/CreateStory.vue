e8aa0c7476105a6743db4fc735d74d0745cd858b
Lily <lily.bickerstaffe@live.co.uk>
2018-09-17 02:58:15 +0530

    Created on success: created story section

--- src/pages/CreateStory.vue
+++ src/pages/CreateStory.vue
@@ -1,17 +1,29 @@
 <template>
     <Jumbotron>
         <span slot="title">
-            <span v-if="createdEverything">Success!</span>
+            <span v-if="createdEverything"></span>
             <span v-else-if="createdStory">Add Tags to Your Story</span>
             <span v-else>Create New Story</span>
         </span>
         <div v-if="createdEverything">
-            Your story has been created:
+            <p>Success! Your work has been created.</p>
+            <h3 class="beta-title">
+                {{ story.title }}
+            </h3>
+            <br>
+            <p>
+                <router-link class="btn btn-lg btn-success" to="/me">
+                    My Works&nbsp;
+                    <i class="fas fa-book-open"></i>
+                </router-link>
+            </p>
         </div>
         <div v-else-if="createdStory">
+            <br>
             <StoryTagForm :storyId="storyId"></StoryTagForm>
         </div>
         <div v-else>
+            <br>
             <StoryForm></StoryForm>
         </div>
   </Jumbotron>
@@ -33,13 +45,18 @@ export default {
         return {
             createdStory: false,
             createdEverything: false,
-            storyId: null
+            storyId: null,
+            story: {}
         }
     },
     mounted() {
         Event.$on('createdStory', story => {
             this.createdStory = true;
             this.storyId = story.story_id;
+            this.story = story;
+        })
+        Event.$on('addedTagsToStory', story => {
+            this.createdEverything = true;
         })
     }
 }
