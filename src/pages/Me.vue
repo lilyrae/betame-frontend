e10946b08b192d53851ced0adf9dd1a7c47356a6
Lily <lily.bickerstaffe@live.co.uk>
2018-12-02 19:49:03 +0900

    Changed styling of my works page

--- src/pages/Me.vue
+++ src/pages/Me.vue
@@ -1,34 +1,47 @@
 <template>
-    <Default>
-        <SearchNavBar/>
-        <ErrorAlert :error="error"/>
-        <!-- list of my stories -->
-        <ul class="list-group list-group-flush">
-        <div v-if="isLoadingPage">
-            <center class="loading-screen">
-                <div class="lds-ripple"><div></div><div></div></div>
-                <div class="lds-ripple"><div></div><div></div></div>
-                <div class="lds-ripple"><div></div><div></div></div>
-            </center>
+    <Wide>
+        <div class="row top-row">
+            <div class="col-md-3 panel">
+                <div class="user-panel text-left">
+                    <h3 class="beta-title account-title">
+                        <i class="fas fa-user-astronaut"></i>&nbsp;{{ username }}
+                    </h3>
+                    <hr class="title-hr">
+                </div>
+            </div>
+            <div class="col-md-8">
+                <SearchNavBar/>
+                <ErrorAlert :error="error"/>
+                <!-- list of my stories -->
+                <ul class="list-group list-group-flush">
+                    <div v-if="isLoadingPage">
+                        <center class="loading-screen">
+                            <div class="lds-ripple"><div></div><div></div></div>
+                            <div class="lds-ripple"><div></div><div></div></div>
+                            <div class="lds-ripple"><div></div><div></div></div>
+                        </center>
+                    </div>
+                    <div v-else-if="stories.length > 0">
+                        <StoryItem v-for="(story, index) in stories" v-bind:key="story.story_id" v-bind:story="story">
+                            <p class="float-right">
+                            <!-- <button class="btn btn-outline-info small-margin">Edit</button> -->
+                            <button class="btn btn-dark btn-sm" @click="deleteStory(story.story_id, index)">Delete</button>
+                            </p>
+                        </StoryItem>
+                    </div>
+                    <div v-else>
+                        Why don't you create a new story?
+                    </div>
+                </ul>
+            </div>
         </div>
-        <div v-else-if="stories.length > 0">
-            <StoryItem v-for="(story, index) in stories" v-bind:key="story.story_id" v-bind:story="story">
-                <p class="float-right">
-                <!-- <button class="btn btn-outline-info small-margin">Edit</button> -->
-                <button class="btn btn-dark btn-sm" @click="deleteStory(story.story_id, index)">Delete</button>
-                </p>
-            </StoryItem>
-        </div>
-        <div v-else>
-            Why don't you create a new story?
-        </div>
-        </ul>
-    </Default>
+    </Wide>
 </template>
 
 <script>
-import Default from '../templates/Default.vue'
+import Wide from '../templates/Wide.vue'
 import SearchNavBar from '../components/SearchNavBar.vue'
+import AccountNavBar from '../components/AccountNavBar.vue'
 import ErrorAlert from '../components/ErrorAlert.vue'
 import StoryItem from '../components/StoryItem.vue'
 import story from '../services/story.js'
@@ -36,8 +49,9 @@ import story from '../services/story.js'
 export default {
     name: 'Me',
     components: {
-        Default,
+        Wide,
         SearchNavBar,
+        AccountNavBar,
         ErrorAlert,
         StoryItem
     },
@@ -46,11 +60,13 @@ export default {
             stories: [],
             user_id: '',
             error: null,
-            isLoadingPage: false
+            isLoadingPage: false,
+            username: ''
         }
     },
     created() {
         this.user_id = localStorage.getItem('bm_user_id');
+        this.username = localStorage.getItem('bm_username');
         this.getStories();
     },
     methods: {
@@ -85,4 +101,32 @@ export default {
 .small-margin {
   margin-right: 5px;
 }
+
+.top-row {
+    margin-top: -10px;
+}
+
+.panel {
+    background-color: #bfdde2!important;
+}
+
+.user-panel {
+    margin: 20px;
+    margin-top: 40px;
+    padding: 20px;
+}
+
+.account-title {
+    -webkit-text-stroke: 1px black;
+    color: white;
+    text-shadow:
+        2px 2px 0 #312e2b,
+        -1px -1px 0 #312e2b,  
+        1px -1px 0 #312e2b,
+        -1px 1px 0 #312e2b,
+        1px 1px 0 #312e2b;
+        overflow: hidden;
+    text-overflow: ellipsis;
+}
+
 </style>
---
+++ src/templates/Wide.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div class="container-fluid">
+    <TopNavBar/>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import TopNavBar from '../components/TopNavBar.vue'
+
+export default {
+  name: 'Full',
+  components: {
+      TopNavBar
+  }
+}
+</script>
