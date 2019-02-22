3579f661bc2c5ab1a0ab2b4635455d81eed2a9fe
Lily <lily.bickerstaffe@live.co.uk>
2019-02-22 21:27:16 +0630

    Fixed leaderboard hiding first user

--- src/pages/LeaderBoard.vue
+++ src/pages/LeaderBoard.vue
@@ -9,7 +9,6 @@
             Here we hope to award our users for all their hard work!
         </p>
     </div>
-
     <div v-if="isLoadingPage">
         <center class="loading-screen">
             <div class="lds-ripple"><div></div><div></div></div>
@@ -17,14 +16,13 @@
             <div class="lds-ripple"><div></div><div></div></div>
         </center>
     </div>
-    <div v-else-if="leaderboard.length > 0">
+    <div v-else-if="firstUser || leaderboard.length > 0">
         <div class="card text-left leaderboard-card">
             <ul class="list-group list-group-flush leaderboard-list-group">
                 <UserRow :user="firstUser" tag="winning" icon="crown" :index="1" />
                 <UserRow v-for="(user, index) in leaderboard" :user="user" :index="index + 2" :key="index" />
             </ul>
         </div>
-
     </div>
     <div v-else>
         No one has given any cookies yet!
@@ -50,7 +48,7 @@ export default {
     data() {
         return {
             leaderboard: [],
-            firstUser: {},
+            firstUser: null,
             error: null,
             isLoadingPage: false
         }
@@ -65,7 +63,7 @@ export default {
 
             user.top()
                 .then(response => {
-                    this.leaderboard = response.data;
+                    this.leaderboard = response.data
                     if (this.leaderboard.length > 0) {
                         this.firstUser = this.leaderboard.shift()
                     }
