7901654127066489c2be9914df6144285a77f08a
Lily <lily.bickerstaffe@live.co.uk>
2019-02-24 23:55:12 +0630

    Added getting user info from endpoint

--- src/layouts/Account.vue
+++ src/layouts/Account.vue
@@ -7,9 +7,16 @@
                         <font-awesome-icon icon="user-astronaut" />&nbsp;{{ username }}
                     </h3>
                     <hr class="title-hr">
-                    <router-link class="btn betame-light-button link" to="/me">My Stories</router-link>
-                    <router-link class="btn betame-light-button link" to="/me/cookies">My Cookies</router-link>
-                    <router-link class="btn betame-light-button link" to="/me/password">Change Password</router-link>
+                    <h5 class="beta-title">
+                        <router-link to="/me" class="beta-link link number-link">
+                            <font-awesome-icon class="font14" icon="book-open" />&nbsp;{{ storyCount }}
+                        </router-link>
+                        <router-link to="/me/cookies" class="beta-link link number-link">
+                            <font-awesome-icon class="golden font14" icon="cookie" />&nbsp;{{ cookieCount }}
+                        </router-link>
+                    </h5>
+                    <hr class="title-hr">
+                    <router-link class="btn betame-light-button beta-link link" to="/me/password">Change Password</router-link>
                 </div>
             </div>
             <div class="col-md-8">
@@ -21,6 +28,7 @@
 
 <script>
 import Wide from '../layouts/Wide.vue'
+import user from '../services/user.js'
 
 export default {
     name: 'Account',
@@ -29,11 +37,23 @@ export default {
     },
     data() {
         return {
-            username: ''
+            username: '',
+            userId: null,
+            cookieCount: null,
+            storyCount: null
         }
     },
     created() {
-        this.username = localStorage.getItem('bm_username')
+        this.userId = localStorage.getItem('bm_user_id')
+
+        user.get(this.userId)
+            .then(response => {
+                if (response.data) {
+                    this.username = response.data.username
+                    this.cookieCount = response.data.karma_count
+                    this.storyCount = response.data.story_count
+                }
+            })
     }
 }
 </script>
@@ -60,6 +80,10 @@ body {
     padding: 20px;
 }
 
+.number-link {
+    text-decoration: none;
+}
+
 .account-title {
     text-overflow: ellipsis;
     -webkit-text-stroke: 1px black;
@@ -73,9 +97,44 @@ body {
     overflow: hidden;
 }
 
+.sub-title {
+    text-overflow: ellipsis;
+    -webkit-text-stroke: 0.25px black;
+    color: white;
+    text-shadow:
+        0.5px 0.5px 0 #312e2b,
+        -0.25px -0.25px 0 #312e2b,  
+        0.25px -0.25px 0 #312e2b,
+        -0.25px 0.25px 0 #312e2b,
+        0.25px 0.25px 0 #312e2b;
+    overflow: hidden;
+}
+
 .fa-user-astronaut path {
   stroke: black;
   stroke-width: 25;
+}
+
+.fa-cookie path {
+  stroke: black;
+  stroke-width: 25;
+}
+
+.fa-book-open path {
+  stroke: black;
+  color: white;
+  stroke-width: 50;
+  -webkit-filter: brightness(100%); /* Safari */
+  filter: brightness(100%);
+}
+
+.fa-book-open path:hover {
+  color: #312e2b;
+  stroke: white;
+}
+
+.fa-cookie path:hover {
+  color: brown;
 }
 
 .link {
--- src/services/user.js
+++ src/services/user.js
@@ -5,5 +5,8 @@ const userUrl = "/user";
 export default {
     top() {
         return api.get(`${userUrl}/top`)
+    },
+    get(userId) {
+        return api.get(`${userUrl}/${userId}`)
     }
 }
