a0783be61297e0b3aeb7381ff842d7ee6f5dc43f
Lily <lily.bickerstaffe@live.co.uk>
2019-03-23 20:41:20 +0700

    Increase minimising

--- src/components/Lists/StoryItem.vue
+++ src/components/Lists/StoryItem.vue
@@ -1,30 +1,30 @@
 <template>
-    <li class="list-group-item beta-text">
+    <li class="list-group-item beta-text" :class="{'pt-0' : minimise, 'pb-0' : minimise}">
         <div class="row">
-        <div class="bullet-point-col">
-            <font-awesome-icon @click="minimise = !minimise" icon="pen-nib" class="betame-red font14 clickable"/>
-        </div>
-        <div class="content-col">
-            <h4 class="hide-overflow row story-title">
-                <span class="col">
-                    <router-link class="beta-title beta-link" :to="storyUrl">{{ story.title }}</router-link>
-                    &nbsp;<span class="font18 grey">by {{ story.user.username }}</span>
-                </span>
-                <span v-show="!minimise" class="beta-title font14 col-md-3">{{ story.created_at | formatDate }}</span>
-            </h4>
-            <div class="font18" v-show="!minimise">
-                <p class="hide-overflow">{{ story.notes }}</p>
-                <p class="small-bottom-margin">
-                    <TagList @clickedTag="searchTag" :tags="story.tags"/>
-                </p>
-                <p class="small-bottom-margin row stats">
-                    <span class="col"></span>
-                    <span class="col-md-3">{{ story.word_count }} words,&nbsp;{{ story.comment_count }} {{ 'comment' | pluralise(story.comment_count) }}</span>
-                    <span class="col"></span>
-                </p>
+            <div class="bullet-point-col">
+                <font-awesome-icon @click="minimise = !minimise" icon="pen-nib" class="betame-red font14 clickable"/>
             </div>
-            <slot></slot>
-        </div>
+            <div class="content-col">
+                <h4 class="hide-overflow row story-title">
+                    <span class="col">
+                        <router-link class="beta-title beta-link" :to="storyUrl">{{ story.title }}</router-link>
+                        &nbsp;<span class="font18 grey">by {{ story.user.username }}</span>
+                    </span>
+                    <span v-show="!minimise" class="beta-title font14 col-md-3">{{ story.created_at | formatDate }}</span>
+                </h4>
+                <div class="font18" v-show="!minimise">
+                    <p class="hide-overflow">{{ story.notes }}</p>
+                    <p class="small-bottom-margin">
+                        <TagList @clickedTag="searchTag" :tags="story.tags"/>
+                    </p>
+                    <p class="small-bottom-margin row stats">
+                        <span class="col"></span>
+                        <span class="col-md-3">{{ story.word_count }} words,&nbsp;{{ story.comment_count }} {{ 'comment' | pluralise(story.comment_count) }}</span>
+                        <span class="col"></span>
+                    </p>
+                </div>
+                <slot></slot>
+            </div>
         </div>
     </li>
 </template>
