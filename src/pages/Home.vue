b55ecde4dcf8740de036d916b489f9462eef6d41
Lily <lily.bickerstaffe@live.co.uk>
2019-05-07 22:45:18 +0700

    Added banner

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,27 +1,32 @@
 <template>
-  <Default>
-    <SearchNavBar/>
-    <ErrorAlert :error="error"/>
-    <div v-if="isLoading">
-        <LoadingRipple />
-    </div>
-    <div v-else-if="filteredStories.length > 0">
-        <p class="text-left help-text text-muted">Search results..</p>
-        <ul class="list-group list-group-flush">
-        <StoryItem v-for="story in filteredStories" v-bind:key="story.story_id" v-bind:story="story"></StoryItem>
-        </ul>
-    </div>
-    <div v-else-if="allStories.length > 0">
-        <!-- list of stories -->
-        <ul class="list-group list-group-flush">
-        <StoryItem v-for="story in allStories" v-bind:key="story.story_id" v-bind:story="story"></StoryItem>
-        </ul>
-    </div>
-    <div v-else>
-        There are no stories here yet!
-    </div>
-    <BottomNavBar />
-  </Default>
+    <Default>
+        <div class="banner jumbotron text-left">
+            <h2>Beta me, Beta you <span><font-awesome-icon icon="heart" /></span></h2>
+            <h5>BetaMe is a place for sharing your writing & giving and receiving feedback.</h5>
+            <h5>The community welcomes all writers, from fan fiction to blog writing!</h5>
+        </div>
+        <SearchNavBar/>
+        <ErrorAlert :error="error"/>
+        <div v-if="isLoading">
+            <LoadingRipple />
+        </div>
+        <div v-else-if="filteredStories.length > 0">
+            <p class="text-left help-text text-muted">Search results..</p>
+            <ul class="list-group list-group-flush">
+            <StoryItem v-for="story in filteredStories" v-bind:key="story.story_id" v-bind:story="story"></StoryItem>
+            </ul>
+        </div>
+        <div v-else-if="allStories.length > 0">
+            <!-- list of stories -->
+            <ul class="list-group list-group-flush">
+            <StoryItem v-for="story in allStories" v-bind:key="story.story_id" v-bind:story="story"></StoryItem>
+            </ul>
+        </div>
+        <div v-else>
+            There are no stories here yet!
+        </div>
+        <BottomNavBar />
+    </Default>
 </template>
 
 <script>
@@ -107,23 +112,51 @@ export default {
     padding: 20px 0px 5px 50px;
 }
 
+.banner {
+    background-color: #bfdde2;
+    font-family: 'Just Another Hand', cursive;
+    padding: 1.2rem 1rem;
+    letter-spacing: 1px;
+    background-image: url('~@/assets/big-scroll.png');
+    background-position: right bottom;
+    background-repeat: no-repeat; 
+    margin-bottom: 10px;
+}
+
+.banner h2 {
+    font-size: 400%;
+    padding-left: 30px;
+    letter-spacing: 2px;
+}
+
+.banner h2 span {
+    font-size: 10%;
+}
+
+.banner h5 {
+    font-family: 'PT Sans', sans-serif;
+    font-weight: 100;
+    padding-left: 30px;
+}
+
+.banner div {
+    padding: 10px 0;
+}
+
 @media (max-width: 768px) {
     .help-text {
         padding: 20px 0px 5px 25px;
     }
-}
 
-.banner {
-    background-color: #bfdde2;
-    font-family: 'Just Another Hand', cursive;
-    padding: 2rem 1rem;
-    letter-spacing: 1px;
-}
-
-@media (min-width: 768px) {
-    .banner {
-        padding: 2rem 2rem;
+    .banner h2 {
+        padding-left: 0px;
     }
 }
 
+@media (max-width: 991px) {
+    .banner {
+        background-image: none;
+    }
+}
 </style>
+
